6e15cb43aa76b9ad5d85e9f4833955d52606327c
mirrari <mirrari@10.0.0.3>
2018-05-01 23:25:54 -0500

    Multidimensional search array test

--- WordSearch/src/WordSearch.java
+++ WordSearch/src/WordSearch.java
@@ -10,10 +10,27 @@ public class WordSearch {
 
 	public String fileName;
 	public List<String> words;
+	public String[][] searchBox;
 
 	public void loadStructuresFromFile() {
 		try (BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)))) {
-			words = Arrays.asList(reader.readLine().split(","));
+			String line = reader.readLine();
+			if(line == null) throw new IOException("File maybe empty, readline returned null"); 
+			words = Arrays.asList(line.split(","));
+			int currentRow = 0;
+			while(line != null) {
+				line = reader.readLine();
+				if(line != null) {
+					String[] row = line.split(",");
+					if(searchBox == null) {
+						searchBox = new String[row.length][row.length];
+					}
+					for(int i = 0; i < row.length; i++) {
+						searchBox[i][currentRow] = row[i];
+					}
+					currentRow++;
+				}
+			}
 		} 
 		catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
--- WordSearch/test/WordSearchTest.java
+++ WordSearch/test/WordSearchTest.java
@@ -3,17 +3,33 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class WordSearchTest {
 
-	@Test
-	public void testLoadWordsFromFile() {
-		WordSearch search = new WordSearch();
+	WordSearch search;
+	
+	@BeforeEach
+	public void setUp() {
+		search = new WordSearch();
 		search.fileName="testData/testFile.txt";
 		search.loadStructuresFromFile();
+	}
+	
+	@Test
+	public void testLoadWordsFromFile() {
 		List<String> expectedWords = Arrays.asList(new String[] {"SCOUT","AVOCADO","BONE","ROPE","STICK","BIKE"});
 		assertEquals(expectedWords,search.words);
+	}
+	
+	
+	@Test
+	public void testLoadMultiDimensionalSearchArray() {
+		assertEquals("A", search.searchBox[18][0]);
+		assertEquals("U", search.searchBox[9][12]);
+		assertEquals("P", search.searchBox[0][18]);
+		assertEquals("E", search.searchBox[18][18]);
 	}
 	
 }
